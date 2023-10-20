5ef2dae214b2c6b457a35efa06f4ad757afc5cf1
mr.w1zard@mail.ru <mr.w1zard@mail.ru>
2023-10-20 18:16:44 +0600

    Add sh5exec func. Request proc to sh5

--- sh5exec.go
+++ sh5exec.go
@@ -1,5 +1,25 @@
 package sh5apiclient
 
-func (c Client) Sh5Exec() {
+import "fmt"
 
+func (c Client) Sh5Exec(procName string, inputData []ShInputData) (any, error) {
+	url := fmt.Sprintf("%s:%d/api/sh5exec", c.BaseURL, c.Port)
+
+	input := Sh5ProcRequest{
+		Sh5BaseRequest: Sh5BaseRequest{
+			Username: c.UserName,
+			Password: c.Password,
+		},
+		ProcName: procName,
+		Input:    inputData,
+	}
+
+	req, err := c.newRequest("POST", url, input)
+	if err != nil {
+		return nil, err
+	}
+
+	var sh5Response Sh5ProcResponse
+	_, err = c.do(req, &sh5Response)
+	return &sh5Response, err
 }
