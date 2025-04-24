1797a00f4d09dabe755ba2ba18ad23c9db6a507d
Alexandr Andreyev <mr.w1zard@mail.ru>
2025-04-24 12:16:58 +0500

    input ReadOdocs change to struct

--- oDocs.go
+++ oDocs.go
@@ -1,26 +1,33 @@
 package sh5apiclient
 
 // Request server settings and database information
-func (c Client) ReadODocs(fromDate, toDate, sunit, corr string) (*Sh5ProcParseResponse, error) {
+type ODocsInput struct {
+	FromDate string
+	ToDate   string
+	Sunit    string
+	Corr     string
+}
+
+func (c Client) ReadODocs(input ODocsInput) (*Sh5ProcParseResponse, error) {
 	// Filtering orders from the request by date
 	filterDate := ShInputData{
 		Head:     "108",
 		Original: []string{"1", "2", "30", "225#2\\1"},
-		Values:   [][]interface{}{{fromDate}, {toDate}, {"480"}, {}}, // 480 ?
+		Values:   [][]interface{}{{input.FromDate}, {input.ToDate}, {"480"}, {}}, // 480 ?
 	}
 
 	// Filtering orders by implementation location (Possible)
 	filterSUnit := ShInputData{
 		Head:     "226#10",
 		Original: []string{"1"},
-		Values:   [][]interface{}{{sunit}},
+		Values:   [][]interface{}{{input.Sunit}},
 	}
 
 	// Filtering orders by correspondent
 	filterCorr := ShInputData{
 		Head:     "107#10",
 		Original: []string{"1"},
-		Values:   [][]interface{}{{corr}},
+		Values:   [][]interface{}{{input.Corr}},
 	}
 
 	inputData := []ShInputData{filterDate, filterSUnit, filterCorr}
